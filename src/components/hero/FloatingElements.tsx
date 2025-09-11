4fe2020e5410001b02a99e18db16b1f3ab24fdfb
pavlohushuliak <pavelgusulak@gmail.com>
2025-09-11 10:31:49 -0500

    fix emojis

--- src/components/hero/FloatingElements.tsx
+++ src/components/hero/FloatingElements.tsx
@@ -2,10 +2,10 @@
 export const FloatingElements = () => {
   return (
     <>
-      <div className="absolute top-28 left-[15%] text-4xl lg:text-5xl animate-bounce delay-1000 z-20">🐕</div>
-      <div className="absolute top-32 right-[20%] text-3xl lg:text-4xl animate-bounce delay-1000 z-20">🐱</div>
-      <div className="absolute bottom-16 left-[20%] text-3xl lg:text-4xl animate-bounce delay-1000 z-20">🐰</div>
-      <div className="absolute bottom-20 right-[15%] text-4xl lg:text-5xl animate-bounce delay-1000 z-20">🐴</div>
+      <div className="absolute top-32 right-2 md:right-[20%] text-3xl lg:text-4xl animate-bounce delay-1000 z-20">🐱</div>
+      <div className="absolute top-32 left-2 md:left-[15%] text-4xl lg:text-5xl animate-bounce delay-1000 z-20">🐕</div>
+      <div className="absolute bottom-20 left-3 md:left-[20%] text-3xl lg:text-4xl animate-bounce delay-1000 z-20">🐰</div>
+      <div className="absolute bottom-20 right-3 md:right-[15%] text-4xl lg:text-4xl animate-bounce delay-1000 z-20">🐴</div>
     </>
   );
 };
